8412c2592ee58f58a6ea8b52e392717264c74f4b
aturant <a.turant@gmail.com>
2022-12-04 01:34:55 +0100

    without export

--- dev_container_run.sh
+++ dev_container_run.sh
@@ -2,7 +2,7 @@
 
 wget -O ./docker-compose.yml https://github.com/aturant/cgspatial-notebook/blob/master/docker-compose.yml
 #location of pg db
-export PG_DATABASE_LOCATION='/media/storespace/postgres/postgis-docker_db' 
-export JUPYTER_WD=pwd 
-docker compose -f ./docker-compose.yml up \
-| tee |grep -w "http://127[^\s]*" -o -m1 > ./link2open.txt
+PG_DATABASE_LOCATION='/media/storespace/postgres/postgis-docker_db' \
+	JUPYTER_WD=pwd \
+	docker compose -f ./docker-compose.yml up \
+		| tee |grep -w "http://127[^\s]*" -o -m1 > ./link2open.txt
