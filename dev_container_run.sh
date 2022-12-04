dbd3353de9367439a69e0a4153ac2f08416e31f1
aturant <a.turant@gmail.com>
2022-12-04 01:24:06 +0100

    qutodownload optiona added

--- dev_container_run.sh
+++ dev_container_run.sh
@@ -1,7 +1,8 @@
 #! /bin/bash
 
+wget -O ./docker-compose.yml https://github.com/aturant/cgspatial-notebook/blob/master/docker-compose.yml
 #location of pg db
 export PG_DATABASE_LOCATION='/media/storespace/postgres/postgis-docker_db' 
 export JUPYTER_WD=pwd 
-docker compose -f /media/storespace/postgres/docker-compose.yml up \
-| tee |grep -w "http://127[^\s]*" -o -m1 > ./link.txt
+docker compose -f ./docker-compose.yml up \
+| tee |grep -w "http://127[^\s]*" -o -m1 > ./link2open.txt
