abdfb81aa0be5589644f69d9ee0d329762ce77a7
aturant <a.turant@gmail.com>
2022-12-06 21:09:21 +0100

    mod of shell script

--- dev_container_run.sh
+++ dev_container_run.sh
@@ -1,6 +1,17 @@
 #! /bin/bash
+echo "Do you wish torun db?"
+select yn in "Yes" "No" "Cancel"; do
+    case $yn in
+        Yes ) FILE = docker-compose.yml; break;;
+        No ) FILE = docker-compose_nodb.yml; break;;
+        Cancel ) exit;;
+    esac
+done
 
-wget -O ./docker-compose.yml https://raw.githubusercontent.com/aturant/cgspatial-notebook/master/docker-compose.yml
+
+
+wget -O ./docker-compose.yml https://raw.githubusercontent.com/aturant/cgspatial-notebook/master/$FILE
+
 #location of pg db
 PG_DATABASE_LOCATION='/media/storespace/postgres/postgis-docker_db' \
 	JUPYTER_WD=pwd \
